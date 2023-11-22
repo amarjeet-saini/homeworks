c001710ba4b40ace6601214c7a80949db13ad92b
amarjeet-saini <amarjeets167@gmail.com>
2023-11-22 14:58:57 +0200

    grammar error removed in output trim strings

--- homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
+++ homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
@@ -8,7 +8,7 @@ using std::endl;
 int main() {
     std::string input{};
     cout << "Example program that trims strings."  << endl;
-    cout << "Please enter a sting:" << endl;
+    cout << "Please enter a string:" << endl;
     std::getline(std::cin, input);
     cout << "Your trimmed string: ";
     std::string trim_strings = no_strings_attached::Trim(input);
